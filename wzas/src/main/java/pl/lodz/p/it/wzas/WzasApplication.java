6ab6c474d8f5c972be02f76cf8c090b2d862b28b
Łukasz Kostrzewa <lukko98@gmail.com>
2020-05-02 13:39:45 +0200

    dodano pusty projekt na backend do elasticSearch

---
+++ wzas/src/main/java/pl/lodz/p/it/wzas/WzasApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.wzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzasApplication.class, args);
+	}
+
+}
